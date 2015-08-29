30194df0f330176d3f772677e96c16d02595b471
veselints <veselintsvetanov@gmail.com>
2015-08-29 19:57:11 +0300

    Initialized working starting point in app.js

---
+++ scripts/app.js
@@ -0,0 +1,10 @@
+// import Track from "scripts/track.js";
+// import Playlist from "scripts/playlist.js";
+// import User from "scripts/user.js";
+// import PlaylistCollection from "scripts/playlistCollection.js";
+
+var appInitialize = function() {
+	window.alert('App loaded sucsessfully!');
+};
+
+export {appInitialize};
