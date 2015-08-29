97ad202bc9a18c9071e70c81fee2131b0a757e5a
veselints <veselintsvetanov@gmail.com>
2015-08-29 20:31:29 +0300

    Implemented the properties of playlistCollection.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,12 @@
-// import Track from "scripts/track.js";
-// import Playlist from "scripts/playlist.js";
-// import User from "scripts/user.js";
-// import PlaylistCollection from "scripts/playlistCollection.js";
+ // import { Track } from 'scripts/track.js';
+ // import { Playlist } from 'scripts/playlist.js';
+ // import { User } from 'scripts/user.js';
+ import { PlaylistCollection } from 'scripts/playlistCollection.js';
 
-var appInitialize = function() {
-	window.alert('App loaded sucsessfully!');
-};
+var appInitialize = (function() {
+	var alert = window.alert('App loaded sucsessfully!');
+	
+	return alert;
+}());
 
 export {appInitialize};
