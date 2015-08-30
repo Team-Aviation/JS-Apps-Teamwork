e8e0fc2e81b391dfb2eaf47d6bc422ba385e46e5
veselints <veselintsvetanov@gmail.com>
2015-08-30 17:48:03 +0300

    Implemented index.html image grid using handlebars.js

--- SoundcloudEnhancer/scripts/app.js
+++ SoundcloudEnhancer/scripts/app.js
@@ -2,7 +2,6 @@
  // import { Playlist } from 'scripts/playlist.js';
  import 'jquery';
  import 'bootstrapjs';
- import 'handlebars';
  import { validator } from 'scripts/validator.js';
  import { track } from 'scripts/track.js';
  import { playlist } from 'scripts/playlist.js';
@@ -13,7 +12,17 @@ var appInitialize = (function() {
 	// Checking if the module is sucsessfully loaded
 	console.log('App loaded sucsessfully!');
 
-	return alert;
+	var imageUrlArray = Array.apply(null, Array(32))
+		.map(function(){
+			return './img/pic385x385.png';
+		});
+
+	var imageUrlContainerObject = {urls: imageUrlArray};
+	var backgroundImageContainer = $('#backgroundContainer');
+    var backgroundImageTemplate = Handlebars.compile(($('#backgroundImageTemplate')).html());
+
+    backgroundImageContainer.html(backgroundImageTemplate(imageUrlContainerObject));
+
 }());
 
 export {appInitialize};
