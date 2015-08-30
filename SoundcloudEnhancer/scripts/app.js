157a4607018b30add5b4f0c654b90d6be2738f4c
veselints <veselintsvetanov@gmail.com>
2015-08-30 21:37:18 +0300

    implemented search results grid

--- SoundcloudEnhancer/scripts/app.js
+++ SoundcloudEnhancer/scripts/app.js
@@ -12,16 +12,40 @@ var appInitialize = (function() {
 	// Checking if the module is sucsessfully loaded
 	console.log('App loaded sucsessfully!');
 
-	var imageUrlArray = Array.apply(null, Array(32))
+	// The below should be replaced by loading an array of the image urls of the last 30 playlists added to the system.
+	var imageUrlArray = Array.apply(null, Array(30))
 		.map(function(){
 			return './img/pic385x385.png';
 		});
-
 	var imageUrlContainerObject = {urls: imageUrlArray};
 	var backgroundImageContainer = $('#backgroundContainer');
-    var backgroundImageTemplate = Handlebars.compile(($('#backgroundImageTemplate')).html());
+    var imageTemplate = Handlebars.compile(($('#imageTemplate')).html());
+    backgroundImageContainer.html(imageTemplate(imageUrlContainerObject));
+	
+	var searchForPlaylists = function() {
+		// This searches the playlist for result according to the words in the input field
+		// 
+		// var allPlaylists = .... load all playlist from db;
+		// var searchedPatternsString = $('#playlystSearcher').val();
+		// var searchedPatternsArra = searchedPatternsString.trim().split(" ");
+		// var searchResults = allPlaylists.searchForPlaylists(searchedPatternsArra);
 
-    backgroundImageContainer.html(backgroundImageTemplate(imageUrlContainerObject));
+		$('#backgroundContainer').css('display', 'none');
+
+		var resultImageUrlArray = Array.apply(null, Array(30))
+		.map(function(){
+			return './img/redPic350x350.png';
+		});
+		var resultImageUrlContainerObject = {urls: resultImageUrlArray};
+		var resultImageContainer = $('#innerContent');
+		// resultImageContainer.css('height', '80%');
+		resultImageContainer.removeClass('cover');
+		resultImageContainer.addClass('searchResultsContainer');
+		resultImageContainer.html(imageTemplate(resultImageUrlContainerObject));
+	};
+
+    $('#searchPlaylistBtn').click(searchForPlaylists);
+
 
 }());
 
