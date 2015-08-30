10644233fb3ed494d71ff897099fd9115c89d165
Gergana Belcheva <gergana.belcheva@gmail.com>
2015-08-30 13:45:25 +0300

    added playlistsSet object

--- scripts/playlistsSet.js
+++ scripts/playlistsSet.js
@@ -1,3 +1,60 @@
-/**
- * Created by GBelcheva on 30/08/2015.
- */
+var playlistsSet = (function(){
+    var validator = require('./validator')();
+
+    var playlistsSetObject = {
+        init: function(playlists) {
+            this.playlists = playlists;
+
+            return this;
+        },
+        get playlists(){
+            return this._playlists;
+        },
+        set playlists(value){
+            validator.validatePlaylists(value, 'Playlists array');
+
+            this._playlists = value;
+        },
+        searchPlaylists: function(patterns){
+            var patternsCaseInsensitive = patterns.map(function(pat){ return pat.toLowerCase();});
+
+            return this.playlists.filter(
+                function(pl){
+                    var foundPlaylist = true;
+
+                    for (var index in patternsCaseInsensitive) {
+                        if (pl.name.toLowerCase().indexOf(patternsCaseInsensitive[index]) < 0) {
+                            foundPlaylist = false;
+                        }
+                    }
+
+                    return foundPlaylist;
+                }
+            );
+        },
+        addNewPlaylist: function(playlist){
+            validator.validateIfUndefined(playlist, 'Playlist to be added');
+            validator.validateIfPlaylist(playlist, 'Playlist to be added');
+
+            this.playlists.push(playlist);
+
+            return this;
+        },
+        deletePlaylist: function(playlist){
+            var playlistExists = this.playlists.some(function(pl){
+                return pl.id === playlist.id;
+            });
+
+            validator.validateIfUndefined(playlist, 'Playlist to be removed');
+            validator.validateIfPlaylist(playlist, 'Playlist to be removed');
+
+            if (playlistExists) {
+                this.playlists.splice(this.playlists.indexOf(playlist, 1));
+            }
+
+            return this;
+        }
+    };
+
+    return playlistsSetObject;
+}());
