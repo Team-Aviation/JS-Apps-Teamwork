aae928670ce4f95a02be736562544317eb56ef99
YordanGergov <yordan.gergov91@gmail.com>
2015-09-01 18:56:57 +0300

    typo

--- SoundcloudEnhancer/scripts/constants.js
+++ SoundcloudEnhancer/scripts/constants.js
@@ -1,6 +1,6 @@
 var constants = (function() {
-    // Checking if the module is sucsessfully loaded
-    console.log('Constants sucsessfully loaded!');
+    // Checking if the module is successfully loaded
+    console.log('Constants successfully loaded!');
 
     return {
         MIN_NAME_LENGTH: 2,
