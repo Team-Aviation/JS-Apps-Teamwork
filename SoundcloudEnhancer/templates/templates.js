c95019c41bb5b8b9198489022c33efc2f2fe8976
RossyB <rossy@compass-bg.net>
2015-09-02 16:54:23 +0300

    Adding home page search
    Make home page continue with others

---
+++ SoundcloudEnhancer/templates/templates.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="../node_modules/handlebars/dist/handlebars.js" />
+/// <reference path="templates.js" />
+
+var templates = (function() {
+    var handlebars = window.handlebars || window.Handlebars,
+      Handlebars = window.handlebars || window.Handlebars;
+
+    function get(name) {
+        var promise = new Promise(function(resolve, reject) {
+            var url = `templates/SearchFormTemplate.html`;
+            console.log(url);
+            $.get(url, function(templateHtml) {
+                var template = handlebars.compile(templateHtml);
+                resolve(template);
+            });
+        });
+        return promise;
+    }
+
+    return {
+        get: get
+   };
+}());
