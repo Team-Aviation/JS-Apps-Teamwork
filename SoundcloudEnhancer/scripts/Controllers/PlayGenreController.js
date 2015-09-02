950e58f0c02085b819c154377d0ba087401af350
GeorgiNik <georgi.nik95@gmail.com>
2015-09-02 16:53:54 +0300

    Controller for genres

---
+++ SoundcloudEnhancer/scripts/Controllers/PlayGenreController.js
@@ -0,0 +1,18 @@
+window.onload = function () {
+    SC.initialize({
+        client_id: '0911f74bb28df4d7fe8d49dc8c14b839'
+
+    });
+    $('.genre').on('click', function (e) {
+        playGenre(e.target.innerHTML);
+    });
+
+};
+function playGenre(genre) {
+    SC.get('/tracks', {genres: genre}, function (tracks) {
+
+        var random =Math.floor(Math.random()*(9-0+1)+0);
+
+        SC.oEmbed(tracks[random].uri, {auto_play: true}, document.getElementById('player'));
+    })
+}
